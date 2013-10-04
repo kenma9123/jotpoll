d46e31a2bad0df418f684562bd75b236eab03054
Kenneth Palaganas <kenma9123@gmail.com>
2013-10-05 03:01:03 +0800

    Removed unnecessary codes from user class

--- lib/classes/User.php
+++ lib/classes/User.php
@@ -50,46 +50,13 @@ Class User
         {
             $user_id = (is_null($uid)) ? $user_data['id'] : $uid;
             $_db = new MySQL(MYSQL_CONNECTION);
-            $stmt = $_db->query("SELECT `id`,`jotform_username`, `jotform_email`, `dropbox_data`, `first_integration` FROM `accounts` WHERE `id` = :id", array('id' => $user_id))->limit(1);
+            $stmt = $_db->query("SELECT `id`,`jotform_username`, `jotform_email` FROM `accounts` WHERE `id` = :id", array('id' => $user_id))->limit(1);
             $user_data = $stmt->fetchAssoc();
         }
 
         //merge session id
         $user_data['user_session'] = Session::getSessionID();
         return $user_data;
-    }
-
-    public static function setDropboxData($data)
-    {
-        $user = User::getUserData();
-        $_db = new MySQL(MYSQL_CONNECTION);
-
-        //save/update the json encoded to db
-        $values = array('dropbox_data' => $data);
-        $where = "`id` = :id AND `jotform_username` = :jotform_username";
-        $binds = array('id' => $user['id'], 'jotform_username' => $user['jotform_username']);
-        $result = $_db->update('accounts', $values, $where, $binds);
-
-        //update session
-        $user['dropbox_data'] = $data;
-        Session::save(self::$sessionVariable, $user);
-    }
-
-    public static function getDropboxTokens($fromUserData = null)
-    {
-        $me = (is_null($fromUserData)) ? self::getUserData(false) : $fromUserData;
-
-        if ( empty($me['dropbox_data']) OR strlen($me['dropbox_data']) == 0 ) {
-            throw new Exception("Dropbox data is empty");
-        }
-
-        //get user data / access tokens
-        $me_dropbox_data = json_decode($me['dropbox_data']);
-
-        return array(
-            'access_token' => $me_dropbox_data->access_token,
-            'access_token_secret' => $me_dropbox_data->access_token_secret
-        );
     }
 
     public function createAccount()
@@ -107,7 +74,7 @@ Class User
         if ( is_null($user) || is_null($email) ) throw new Exception("Username and email are missing");
 
         $binds = array('j_u' => $user, 'j_e' => $email);
-        $queryStatement = "SELECT `id`,`jotform_username`, `jotform_email`, `dropbox_data`, `first_integration` FROM `".$this->tablename."` WHERE `jotform_username` = :j_u AND `jotform_email` = :j_e LIMIT 1";
+        $queryStatement = "SELECT `id`,`jotform_username`, `jotform_email` FROM `".$this->tablename."` WHERE `jotform_username` = :j_u AND `jotform_email` = :j_e LIMIT 1";
         $stmt = $this->_db->prepare($queryStatement);
         $stmt->execute($binds);
         $userData = $stmt->fetchAssoc();
@@ -150,39 +117,6 @@ Class User
                 return "Account successfully created with accountID: " . $accountID . " AND " . $logged_in;
             }
         }
-	}
-
-    public static function _flagUserFirstimeIntegration()
-    {
-        $user = User::getUserData();
-        $_db = new MySQL(MYSQL_CONNECTION);
-
-        $values = array('first_integration' => 0);
-        $where = "`id` = :id AND `jotform_username` = :jotform_username";
-        $binds = array('id' => $user['id'], 'jotform_username' => $user['jotform_username']);
-        $result = $_db->update('accounts', $values, $where, $binds);
-    }
-
-    public static function saveAccessToken( $dropbox = null )
-    {
-        if ( !self::isLoggedIn() ) {
-            throw new Exception("User is not logged in");
-        }
-
-        if ( is_null($dropbox) ) {
-            throw new Exception("Dropbox data is required");
-        }
-
-        $user = self::getUserData(false);
-        $values = array('dropbox_data' => $dropbox);
-        $db = new MySQL(MYSQL_CONNECTION);
-        $result = $db->update('accounts', $values, 'id = ?', array($user['id']));
-
-        //once dropbox_data is updated merge it to session for latest user data
-        $latest_userData = array_merge( $user, $values );
-        Session::save(self::$sessionVariable, $latest_userData);
-
-        return ($result) ? $dropbox : false;
     }
 
     private function updateUser_APIkey()
